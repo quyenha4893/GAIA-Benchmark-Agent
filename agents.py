be7675ef55a839034b5cdd0dd7e894b2744d725a
ssgrummons <ssgrummo@us.ibm.com>
2025-04-28 11:43:02 -0400

    Cleaned up code and improved ReadMe

--- agents.py
+++ agents.py
@@ -1,12 +1,12 @@
-import smolagents.models as sm_models
+# import smolagents.models as sm_models
 
-_orig_roles = sm_models.MessageRole.roles
+# _orig_roles = sm_models.MessageRole.roles
 
-@classmethod
-def _roles_with_control(cls):
-    return _orig_roles() + ["control"]
+# @classmethod
+# def _roles_with_control(cls):
+#     return _orig_roles() + ["control"]
 
-sm_models.MessageRole.roles = _roles_with_control
+# sm_models.MessageRole.roles = _roles_with_control
 
 
 
@@ -28,26 +28,28 @@ import os
 from dotenv import load_dotenv
 import time
 
+load_dotenv()
+
 # Load prompts from YAML file
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
 
 
-class ThinkingLiteLLMModel(LiteLLMModel):
-    def __init__(self, *args, **kwargs):
-        # ensure the Litellm client also maps "control" → "control"
-        cr = kwargs.pop("custom_role_conversions", {})
-        cr["control"] = "control"
-        super().__init__(*args, custom_role_conversions=cr, **kwargs)
+# class ThinkingLiteLLMModel(LiteLLMModel):
+#     def __init__(self, *args, **kwargs):
+#         # ensure the Litellm client also maps "control" → "control"
+#         cr = kwargs.pop("custom_role_conversions", {})
+#         cr["control"] = "control"
+#         super().__init__(*args, custom_role_conversions=cr, **kwargs)
 
-    def __call__(self, messages, **kwargs) -> ChatMessage:
+#     def __call__(self, messages, **kwargs) -> ChatMessage:
         # NOTE: content must be a list of {type, text} dicts
-        thinking_msg = {
-            "role": "control",
-            "content": [{"type": "text", "text": "thinking"}]
-        }
-        # prepend onto whatever messages the Agent built
-        return super().__call__([thinking_msg] + messages, **kwargs)
+#         thinking_msg = {
+#             "role": "control",
+#             "content": [{"type": "text", "text": "thinking"}]
+#         }
+#         # prepend onto whatever messages the Agent built
+#         return super().__call__([thinking_msg] + messages, **kwargs)
     
 class SlowLiteLLMModel(LiteLLMModel):
     def __init__(self, *args, **kwargs):
@@ -58,42 +60,42 @@ class SlowLiteLLMModel(LiteLLMModel):
         # prepend onto whatever messages the Agent built
         return super().__call__(messages, **kwargs)
 
-# search_model_name = 'granite3.3:latest'
-search_model_name = 'cogito:14b'
-# search_model_name = 'qwen2:7b'
-search_model = ThinkingLiteLLMModel(model_id=f'ollama_chat/{search_model_name}',
-                             flatten_messages_as_text=True)
+# # search_model_name = 'granite3.3:latest'
+# search_model_name = 'cogito:14b'
+# # search_model_name = 'qwen2:7b'
+# search_model = ThinkingLiteLLMModel(model_id=f'ollama_chat/{search_model_name}',
+#                              flatten_messages_as_text=True)
 
-web_agent = CodeAgent(
-    model=search_model,
-    tools=[DuckDuckGoSearchTool(), VisitWebpageTool(), FinalAnswerTool()],
-    max_steps=6,
-    verbosity_level=1,
-    grammar=None,
-    planning_interval=6,
-    name="web_agent",
-    description="Searches the web using the and reviews web pages to find information.",
-    additional_authorized_imports=['bs4', 'requests', 'io', 'wiki'],
-    prompt_templates=prompt_templates
-)
+# web_agent = CodeAgent(
+#     model=search_model,
+#     tools=[DuckDuckGoSearchTool(), VisitWebpageTool(), FinalAnswerTool()],
+#     max_steps=6,
+#     verbosity_level=1,
+#     grammar=None,
+#     planning_interval=6,
+#     name="web_agent",
+#     description="Searches the web using the and reviews web pages to find information.",
+#     additional_authorized_imports=['bs4', 'requests', 'io', 'wiki'],
+#     prompt_templates=prompt_templates
+# )
 
-image_model_name = 'llama3.2-vision'
-image_model = OpenAIServerModel(model_id=image_model_name,
-                                api_base='http://localhost:11434/v1/',
-                                api_key='ollama',
-                            flatten_messages_as_text=False)
-image_agent = ToolCallingAgent(
-    model=image_model,
-    tools=[FinalAnswerTool()],
-    max_steps=4,
-    verbosity_level=2,
-    grammar=None,
-    planning_interval=6,
-    #additional_authorized_imports=["PIL", "requests", "io", "numpy"],
-    name="image_agent",
-    description="Review images and videos for answers to questions based on visual data",
-    prompt_templates=prompt_templates
-)
+# image_model_name = 'llama3.2-vision'
+# image_model = OpenAIServerModel(model_id=image_model_name,
+#                                 api_base='http://localhost:11434/v1/',
+#                                 api_key='ollama',
+#                             flatten_messages_as_text=False)
+# image_agent = ToolCallingAgent(
+#     model=image_model,
+#     tools=[FinalAnswerTool()],
+#     max_steps=4,
+#     verbosity_level=2,
+#     grammar=None,
+#     planning_interval=6,
+#     #additional_authorized_imports=["PIL", "requests", "io", "numpy"],
+#     name="image_agent",
+#     description="Review images and videos for answers to questions based on visual data",
+#     prompt_templates=prompt_templates
+# )
 
 # react_model_name = 'mistral-small3.1:24b'
 # # Initialize the chat model
